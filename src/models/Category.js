0252557df7d934f5c9a46a0e683d63b631d6564f
Marcelo De Lima <delima_marcelo@outlook.com>
2022-09-23 19:10:26 -0300

    Feat: Model Category

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+const CategorySchema = (sequelize, DataTypes) => {
+    const CategoryTable = sequelize.define(
+      "Category",
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+        },
+        name: DataTypes.STRING,
+      },
+      {
+        timestamps: false,
+        tableName: "categories",
+      }
+    );
+    return CategoryTable;
+  };
+  
+  module.exports = CategorySchema;
+  
