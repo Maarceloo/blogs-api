eb13104a356b1d3e2c0b6319f5631eda47a5a499
Marcelo De Lima <delima_marcelo@outlook.com>
2022-09-24 14:39:52 -0300

    Feat: Rota User desenvolvida no app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const loginRoute = require('./routes/login.routes');
-
+const userRoute = require('./routes/user.routes');
 // ...
 
 const app = express();
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/login', loginRoute);
+app.use('/user', userRoute);
 
 // ...
 
